2c6d36ecefedd19b07b0dcafe1d7ab8aeab21d5b
satish kashyap <satish.kashyap@uneecops.com>
2022-12-30 18:19:54 +0530

    react native web app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'red',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import {AppRegistry} from 'react-native';
+import App from './App';
+import appName from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
+AppRegistry.runApplication(appName, {
+  rootTag: document.getElementById('root'),
+});
